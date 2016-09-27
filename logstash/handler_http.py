cb6337a81fef38ba3c2356a20a5b8a548dcada50
José Antonio Martín <jose.martin@reclamador.es>
2016-09-27 15:54:00 +0200

    Used requests library instead of httplib for HTTP handler.

--- logstash/handler_http.py
+++ logstash/handler_http.py
@@ -8,7 +8,7 @@ from logstash import formatter
 class HTTPLogstashHandler(logging.Handler, object):
     """
     Python logging handler for Logstash. Sends events over HTTP.
-    :param host: The host of the logstash server.
+    :param url: The url of the logstash server API endpoint.
     :param api_key: The logstash api authorization key.
     :param message_type: The type of the message (default logstash).
     :param fqdn; Indicates whether to show fully qualified domain name or not (default False).
@@ -16,9 +16,9 @@ class HTTPLogstashHandler(logging.Handler, object):
     :param tags: list of tags for a logger (default is None).
     """
 
-    def __init__(self, host, api_key, message_type='logstash', tags=None, fqdn=False, version=0):
+    def __init__(self, url, api_key, message_type='logstash', tags=None, fqdn=False, version=0):
         super(HTTPLogstashHandler, self).__init__()
-        self.host = host
+        self.url = url
         self.api_key = api_key
         if version == 1:
             self.formatter = formatter.LogstashFormatterVersion1(message_type, tags, fqdn)
@@ -28,21 +28,14 @@ class HTTPLogstashHandler(logging.Handler, object):
     def emit(self, record):
         """
         Emit a record.
-        POST the record to the logstash server as JSON data.
+        POST the record to the logstash server API endpoint as JSON data.
         """
         try:
-            import httplib
+            import requests
 
-            h = httplib.HTTPConnection(self.host)
+            headers = {'ApiKey': api_key, 'Content-type': 'application/json'}
             data = json.dumps(record.__dict__)
 
-            h.putrequest('POST', '/')
-            h.putheader("Host", self.host)
-            h.putheader("Content-type", "application/json")
-            h.putheader("ApiKey", self.api_key)
-            h.endheaders()
-
-            h.send(data.encode('utf-8'))
-            h.getresponse()    #can't do anything with the result
+            r = requests.post(self.url, data=data, headers=headers)
         except Exception:
             self.handleError(record)
